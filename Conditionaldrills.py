3d6370978b937076fc22cd4244f5d5ffadf332db
anna463 <57601829+anna463@users.noreply.github.com>
2020-02-02 16:15:21 -0600

    Add files via upload

---
+++ Conditionaldrills.py
@@ -0,0 +1,43 @@
+'''
+Created on Feb 2, 2020
+
+@author: ITAUser
+'''
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+'''
+
+'''
+#1) Create a Variable called grade and set it to an integer. Make an if statement that checks if the grade is a passing grade. grade must be above 65 to pass. print out "student is passing"
+'''
+grade = 68
+
+if (grade == 68):
+    print ("student is passing")
+
+'''
+#2) Now make an if, else statement that checks if the student is passing but also print "student is failing", if the grade is less than 65
+'''
+grade = 68
+
+if (grade == 68):
+    print ("student is passing")
+else:
+    print ("student is failing")
+'''
+#3)Create a variable called age. Make and if, else statement that checks if the age entered is old enough to vote. Remember the voting age is 18
+'''
+age = 18 
+
+if (age == 18):
+    print ("can vote")
+else:
+    print ("can't vote")
+
+'''
+#4)Create a variable called weight. Make an if statement that checks the unit of the weight. If the weight is in kilograms, convert it to pounds 
+'''
+Weight = kilograms
+if (weight == kilograms):
+    print ("pounds") 
+'''
